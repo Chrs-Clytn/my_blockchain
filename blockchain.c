ffc4c459df684384165cc8889d1f9b0f4a945d08
Chris <theperfectexample@gmail.com>
2022-07-14 10:25:29 +0200

    added a remove nodes function, will require testing once everything is linked

--- blockchain.c
+++ blockchain.c
@@ -1,13 +1,13 @@
 #include "blockchain.h"
 
-node_t *append_node(node_t *node_head, int n_id) //this appends to the node struct, takes the node_head and node id. 
+node_t *append_node(node_t *node_head, int n_id) // this appends to the node struct, takes the node_head and node id.
 {
     if (node_head->nId == NULL) // if node_head is empty, fill and return node_head
     {
         node_head->nId = n_id;
         node_head->next = NULL;
         return node_head;
-    } 
+    }
     while (node_head->next != NULL) // otherwise loop through list until we reach the last entry
         node_head = node_head->next;
     node_t *new = (node_t *)malloc(sizeof(node_t)); // allocate memory to a new node and fill with node id.
@@ -51,9 +51,41 @@ void sorter(block_t *block_head, node_t *node_head, int n_id, char *b_id) // sor
     {
         while (current->next != NULL)
         {
-            current = node_head->next;
             if (current->nId == n_id) // if node id matches the input to the function then we continue
                 append_block(block_head, b_id);
+            current = node_head->next;
+        }
+    }
+}
+
+void remove_nodes(node_t *node_head, int n_id) // function to remove nodes based on node id.
+{
+    node_t *temp, *current, *prev;
+    char *comp = atoi(n_id);
+    if (strcmp(comp, "*")) // if node id == * delete all nodes
+    {
+        current = node_head;
+        while (current != NULL) // this should loop through all nodes in list, setting the current->next to NULL and freeing the current. not convinced this will work. Need to test once stuff is up and running
+        {
+            temp = current->next;
+            current->next = NULL;
+            free(node_head);
+            current = temp;
+        }
+    }
+    else // if node id isnt * then need to loop through node list until find correct node id
+    {
+        while (node_head != NULL)
+        {
+            if (node_head->next->nId == n_id) // checks if the next node id is what we need, if it is set head to prev, next to temp, current to next-next and then set temp nxt to NULL and free temp. Also will need testing
+            {
+                prev = node_head;
+                temp = node_head->next;
+                current = node_head->next->next;
+                prev->next = current;
+                temp->next = NULL;
+                free(temp);
+            }
         }
     }
 }
